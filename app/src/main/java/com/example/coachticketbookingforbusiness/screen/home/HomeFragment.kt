6a6a304fc861978195f9cc3e44ae20105f272bd4
hoangpm <hoangd16ptit@gmail.com>
2020-12-09 23:44:45 +0700

    [Feature] Implement qr scan to get ticket id.

--- app/src/main/java/com/example/coachticketbookingforbusiness/screen/home/HomeFragment.kt
+++ app/src/main/java/com/example/coachticketbookingforbusiness/screen/home/HomeFragment.kt
@@ -2,14 +2,13 @@ package com.example.coachticketbookingforbusiness.screen.home
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
 import com.example.coachticketbookingforbusiness.R
 import com.example.coachticketbookingforbusiness.base.BaseFragment
+import com.example.coachticketbookingforbusiness.screen.qr_scan.QRScanFragment
 import com.example.coachticketbookingforbusiness.screen.routes.RoutesFragment
-import com.example.coachticketbookingforbusiness.utils.SharePreferenceUtils
+import es.dmoral.toasty.Toasty
 import kotlinx.android.synthetic.main.home_fragment.*
 
 class HomeFragment : BaseFragment(), View.OnClickListener {
@@ -36,16 +35,26 @@ class HomeFragment : BaseFragment(), View.OnClickListener {
     override fun initListener() {
         containerRoutes.setOnClickListener(this)
         containerProfile.setOnClickListener(this)
+        containerQRScan?.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         when (v) {
             containerRoutes -> {
 //                SharePreferenceUtils.getLocalUserInformation(context)?.let {
-                    val routeFragment = RoutesFragment.newInstance("0900888337")
-                    pushFragment(routeFragment, withAnimation = true, tag = "Route")
+                val routeFragment = RoutesFragment.newInstance("0900888337")
+                pushFragment(routeFragment, withAnimation = true, tag = "Route")
 //                }
             }
+
+            containerQRScan -> {
+                val qrScanFragment = QRScanFragment.newInstance()
+                qrScanFragment.setOnReceiveTicketId {
+                    context?.let { it1 -> Toasty.success(it1, it, Toast.LENGTH_LONG, true).show() }
+                }
+                pushFragment(qrScanFragment, withAnimation = false)
+            }
+
         }
     }
 
